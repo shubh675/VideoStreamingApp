26b16931cb0f171c5172a27993298515ae02bd53
Shubham Gehlot <shubhamgehlot675@gmail.com>
2023-04-21 08:26:13 +0530

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Button, Box } from "@mui/material";
 const App=() => {
   return <div className="text-3xl m-8">App
   <h3>making some minore changes</h3>
+  <h2>more changes</h2>
   </div>;
 };
 export default App;
