aba0994460cb64ce60a0a33f41aac6fd547ccc8d
Shubham Gehlot <shubhamgehlot675@gmail.com>
2023-04-21 08:24:53 +0530

    changes tp app.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Button, Box } from "@mui/material";
 
 const App=() => {
-  return <div className="text-3xl m-8">App</div>;
+  return <div className="text-3xl m-8">App
+  <h3>making some minore changes</h3>
+  </div>;
 };
 export default App;
